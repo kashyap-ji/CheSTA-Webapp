8c75a6c191bf3a2781261be45306798bb6a69bba
Sayan Dey Sarkar <sayandeysarkar2003@gmail.com>
2023-07-07 04:18:02 +0530

    updates

--- frontend/src/components/footer/message.js
+++ frontend/src/components/footer/message.js
@@ -6,8 +6,10 @@ function MessageUs(){
         <div>
             <div className="header">Message Us</div>
             <form action="" method="post">
+                <div style={{position: "relative"}} >
                 <input type="text" name="message" id="message" placeholder="Message" className="box" />
                 <button type="sumit" className="Button">Submit</button>
+                </div>
             </form>
         </div>
     )
