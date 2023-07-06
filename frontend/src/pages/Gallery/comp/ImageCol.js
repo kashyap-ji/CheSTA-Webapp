08f4cd7b3537eacfb58cc95fcc33e8371908d416
Siddharth Kashyap <kashyapsidiitg@gmail.com>
2023-07-07 01:22:00 +0530

    added Gallery section in pages exact CSS pending

---
+++ frontend/src/pages/Gallery/comp/ImageCol.js
@@ -0,0 +1,15 @@
+import './ImageCol.css';
+
+
+
+const ImageCol = () => {
+    return (<div className='imageCol'>
+        <div className='image'></div>
+        <div className='image'></div>
+        <div className='image'></div>
+        <div className='image'></div>
+
+    </div>)
+}
+
+export default ImageCol;
